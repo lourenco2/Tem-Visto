888d26cebbb65c53349256cd7027793197446ae5
Flavyanne Leonardo <flavyleo@gmail.com>
2023-02-24 10:32:41 -0300

    config firebase

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require("express") //função que cria um servidor express
 const rotaLocalizacao = require("./routes/localizacao")
+import admin from 'firebase-admin'
 
 const app = express() //Importação do servidor para o arquivo principal do nosso projeto
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
 
 app.use('/localizacao', rotaLocalizacao)
 
